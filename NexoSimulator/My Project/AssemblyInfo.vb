2f8b4531c99b0dde4e2fbef203c45a2a2ecfd5ad
pmespace <philippemp31@outlook.com>
2020-10-07 18:36:10 +0200

    Removed ghosts files

--- NexoSimulator/My Project/AssemblyInfo.vb
+++ NexoSimulator/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("2020.281.0832.0")>
-<Assembly: AssemblyFileVersion("2020.281.0832.0")>
+<Assembly: AssemblyVersion("2020.281.1634.0")>
+<Assembly: AssemblyFileVersion("2020.281.1634.0")>

59bad0b1da9fff79a1053d2511b987dbf08e0e26
pmespace <philippemp31@outlook.com>
2020-10-19 19:11:34 +0200

    Nuget update

--- NexoSimulator/My Project/AssemblyInfo.vb
+++ NexoSimulator/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("2020.293.1654.0")>
-<Assembly: AssemblyFileVersion("2020.293.1654.0")>
+<Assembly: AssemblyVersion("2020.293.1711.0")>
+<Assembly: AssemblyFileVersion("2020.293.1711.0")>

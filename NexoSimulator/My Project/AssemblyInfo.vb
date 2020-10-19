6b625eb9e63ff81a239220123062cdc8ac7d734b
pmespace <philippemp31@outlook.com>
2020-10-19 18:57:45 +0200

    Version 2.0.1 for DLLs

--- NexoSimulator/My Project/AssemblyInfo.vb
+++ NexoSimulator/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("2.0.7597.3")>
-<Assembly: AssemblyFileVersion("2.0.7597.3")>
+<Assembly: AssemblyVersion("2020.293.1654.0")>
+<Assembly: AssemblyFileVersion("2020.293.1654.0")>

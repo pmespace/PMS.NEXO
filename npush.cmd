43d7918d239e5f98f552c2fee49a026def6a3b06
philippemp31@outlook.com <pmespace>
2022-05-12 11:22:13 +0200

    v4.6.0 - push updated

--- npush.cmd
+++ npush.cmd
@@ -1,3 +1,4 @@
 @echo off
-nuget push common\bin\*.nupkg  -Source https://api.nuget.org/v3/index.json
+nuget push nexo.30\bin\*.nupkg  -Source https://api.nuget.org/v3/index.json
+nuget push nexo.31\bin\*.nupkg  -Source https://api.nuget.org/v3/index.json
 pause
